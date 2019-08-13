73a6ea58d61c3bba5a575f50e5a74bf02f44a83f
hugozhangc <48342044+hugozhangc@users.noreply.github.com>
2019-08-13 09:27:54 +0800

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,15 @@
+
+
+ciphertext = 'iehho de cehu sxesebqju rqhi'
+
+for i in range(0,26):
+	sentence = ''
+	for ch in ciphertext:
+		if ch == ' ':
+			new_ch = ord(ch)
+		else:
+			new_ch = ord(ch) - 1
+		sentence += chr(new_ch)
+	print(ciphertext)
+
+	ciphertext = sentence
